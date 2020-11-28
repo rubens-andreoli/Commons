2ef704fd34c3b81f82930f75bc7a6501a5ae3026
Rubens A. Andreoli Jr. <rubens.andreoli@gmail.com>
2020-11-27 21:10:59 -0300

    Add: NumberUtils class to replace IntegerUtils

--- src/main/java/rubensandreoli/commons/utils/IntegerUtils.java
+++ src/main/java/rubensandreoli/commons/utils/IntegerUtils.java
@@ -18,27 +18,17 @@
  */
 package rubensandreoli.commons.utils;
 
+@Deprecated
 public class IntegerUtils {
     
-    /**
-     * Parses the string argument as a signed decimal integer.
-     * Convenience method for {@code Integer#parseInt()} that doesn't
-     * throw {@code NumberFormatException}.
-     * 
-     * @param value a {@code String} containing the {@code int} to be parsed
-     * @return the integer value or {@literal 0} if exception is thrown
-     */
+    @Deprecated
     public static int parseInteger(String value){
-        try{
-            return Integer.parseInt(value);
-        }catch(NumberFormatException ex){
-            return 0;
-        }
+        return NumberUtils.parseInteger(value);
     }
     
+    @Deprecated
     public static int getRandomBetween(int min, int max){
-        if(max < min) max = min;
-        return (int) (Math.random() * (max - min)) + min;
+        return NumberUtils.getRandomBetween(min, max);
     }
 
 }
